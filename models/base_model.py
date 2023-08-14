4974115ad0d0fb66f23277374af8508514d1bb21
talent <124376251+CourageT1@users.noreply.github.com>
2023-08-14 20:42:12 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,53 +1,34 @@
 #!/usr/bin/python3
 """ class BaseModel """
-from uuid import uuid4
+import uuid
 from datetime import datetime
 import models
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        """
-        Constructor for BaseModel.
-        """
         if kwargs:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key in ['created_at', 'updated_at']:
-                    setattr(self, key, datetime.strptime(value))
-                else:
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
                     setattr(self, key, value)
+                    storage.new(self)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-
-    def save(self):
-        """
-        Update the 'updated_at' attribute with the current datetime.
-        """
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        """
-        Convert the instance attributes to a dictionary representation.
-
-        Returns:
-            dict: A dictionary containing the instance attributes.
-        """
-        instance_dict = self.__dict__.copy()
-        instance_dict['__class__'] = self.__class__.__name__
-        instance_dict['created_at'] = self.created_at.isoformat()
-        instance_dict['updated_at'] = self.updated_at.isoformat()
-        return instance_dict
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-        Get a human-readable string representation of the instance.
-
-        Returns:
-            str: A formatted string representing the instance.
-        """
         return "[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, self.__dict__)
+            self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
